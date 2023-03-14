029828d8c0df7f2691b6988ee0ef5df44e06e87b
Kim-Daejung <91stylepassion@gmail.com>
2023-03-14 19:29:00 +0900

    Study_

---
+++ keras/Prac/wine_pra.py
@@ -0,0 +1,30 @@
+from tensorflow.python.keras.models import Sequential, Model
+from tensorflow.python.keras.layers import Dense, Input, Dropout
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.callbacks import EarlyStopping
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import accuracy_score
+from keras.utils import to_categorical
+import pandas as pd
+import numpy as np
+
+#1. 데이터
+path = './_data/dacon_wine/'
+path_save = './_save/dacon_wine/'
+
+train_csv = pd.read_csv(path + 'train.csv',
+                        index_col=0)
+
+print(train_csv) # 5497, 13
+
+test_csv = pd.read_csv(path + 'test.csv',
+                       index_col=0)
+
+print(test_csv) # 1000, 12
+
+print(type(train_csv)) # 데이터 타입을 알 수 있다
+
+0부터 시작하는건 to_categorical
+get_dumi는 바로 바뀐다
+
+원핫은 변환해줘야한다
