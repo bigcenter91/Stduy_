929acab07ce8f8a78b1c06365e48911f32095912
bigcenter91 <91stylepassion@gmail.com>
2023-06-06 23:53:07 +0900

    0606_

--- yolo/test.py
+++ yolo/test.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
 
-video_path = 'C:\study_data\_data\_ydata\g20230526_154008.mp4'
+video_path = 'C:\study_data\_data\_ydata\ss20230605_224916.mp4'
 cap = cv2.VideoCapture(video_path)
 # cap = cv2.VideoCapture(0)
 
